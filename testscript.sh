428e3aed6d5debb935ef4cac1943992fd7ca3c9c
Naverick <naveedrahman1@gmail.com>
2021-01-28 14:04:04 -0500

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "this script is from github"
+echo "Par Numberone is $NUMBERONE"
+echo "Par Numbertwo is $NUMBERTWO"
 Number=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "this is complete"
